417af23ad03aa619ea048d2a2982bb341ca4ee51
José Juan Rivera Merino <juanrivmerino4@gmail.com>
2024-02-27 10:11:18 -0600

    2_5 manejo y optimizacion de imagenes

--- src/components/home/Description/Description.tsx
+++ src/components/home/Description/Description.tsx
@@ -1,9 +1,30 @@
+import styles from './Description.module.sass';
+import Image from 'next/image';
+
+
 export const Description = () => {
     return(
-        <section>
-            <img src="/images/910c97ae-e930-4cfa-afe0-67dba6a620ae.jpg" alt="products marketplace" />
-            <h2>Description</h2>
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sunt possimus iusto harum, quam maiores sapiente? Harum vero assumenda quae! Iste ducimus quae tempore cupiditate itaque autem corporis laboriosam officia maiores.</p>
+        <section className={styles.Description}>
+
+            <Image
+             src="/images/910c97ae-e930-4cfa-afe0-67dba6a620ae.jpg" 
+
+             alt="products marketplace" 
+             width={500} 
+             height={300}
+            
+             
+             />
+             
+
+            <div className={styles.Description__text}> 
+
+                <h2>Description</h2>
+
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sunt possimus iusto harum, quam maiores sapiente? Harum vero assumenda quae! Iste ducimus quae tempore cupiditate itaque autem corporis laboriosam officia maiores.</p>
+
+            </div>
+
         </section>
     )
 }
