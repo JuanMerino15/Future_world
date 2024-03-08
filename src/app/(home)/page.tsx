2e73c15a6e7e4d2d06aa8763dc59d331cd392bd9
José Juan Rivera Merino <juanrivmerino4@gmail.com>
2024-03-07 21:17:12 -0600

    3_5 Route grouping

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,18 +1,10 @@
-
 import { Description } from "app/components/home/Description";
 import { Hero } from "app/components/home/Hero";
 import { MainProducts } from "app/components/home/MainProducts";
-
 export default function Home() {
-  
   return (
     <main>
-     <Hero/>
-
-     <Description/>
-
-     <MainProducts/>
-     
+      <MainProducts/>
     </main>
   );
 }
