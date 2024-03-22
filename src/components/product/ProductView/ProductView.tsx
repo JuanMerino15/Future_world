9797189da4f2e3c646578d5cddfabefa9eca0d15
José Juan Rivera Merino <juanrivmerino4@gmail.com>
2024-03-22 11:38:07 -0600

    4_14 HTML dinamico en la descripción del producto

--- src/components/product/ProductView/ProductView.tsx
+++ src/components/product/ProductView/ProductView.tsx
@@ -1,7 +1,8 @@
 import Image from "next/image";
 import { ProductViewItemsOrder } from "./ProductViewItemsOrder";
 import styles from './ProductView.module.sass'
-import { useRouter } from "next/navigation";
+import { SanitizeHTML } from "app/components/shared/SanitizeHTML/SanitizeHTML";
+
 
 interface ProductViewProps {
   product: ProductType
@@ -9,11 +10,7 @@ interface ProductViewProps {
 
 export const ProductView = ({ product }: ProductViewProps) => {
 
-  const router = useRouter()
 
-  if(!product){
-    router.push('/')
-  }
   return (
     <main className={styles.ProductView}>
       <section className={styles.ProductView__images}>
@@ -29,9 +26,9 @@ export const ProductView = ({ product }: ProductViewProps) => {
       <section className={styles.ProductView__info}>
         <h1 className={styles.ProductView__info__title}>{product.title}</h1>
         <p className={styles.ProductView__info__category}>{product.tags}</p>
-        <p className={styles.ProductView__info__description}>
-          {product.description}
-        </p>
+        <SanitizeHTML tag="p">
+        {product.description}
+        </SanitizeHTML>
         <span className={styles.ProductView__info__price}>
           $ {product.price}
         </span>
