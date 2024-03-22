0d2dbcaa0a4bf0b0fcf2c15239fc9c532746617a
José Juan Rivera Merino <juanrivmerino4@gmail.com>
2024-03-22 12:09:28 -0600

    4_15 Mejorando SEO de una pagina

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,14 +1,15 @@
-import { Description } from "app/components/home/Description";
-import { Hero } from "app/components/home/Hero";
 import { MainProducts } from "app/components/home/MainProducts";
+import { Metadata } from "next";
+export const metadata: Metadata = {
+  title: "Future World 🌟",
+  description: "Welcome to the future world",
+  keywords: ["ecommerce", "future", "world"]
+}
 export default function Home() {
-
   return (
     <main>
-      {/* <h1>product</h1> */}
-
-      <MainProducts/> 
-
+      <h1>Products</h1>
+      <MainProducts/>
     </main>
   );
 }
