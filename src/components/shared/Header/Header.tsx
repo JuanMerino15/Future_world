468e22c69ee4483f5ed1136efa390c0f7f087182
José Juan Rivera Merino <juanrivmerino4@gmail.com>
2024-04-16 16:33:19 -0600

    5_10 Como integrar el checkout de shopify

--- src/components/shared/Header/Header.tsx
+++ src/components/shared/Header/Header.tsx
@@ -1,7 +1,9 @@
 import Link from 'next/link'
-import { ShoppingCart } from '../ShoppingCart'
+import dynamic from 'next/dynamic'
 import { validateAccessToken } from 'app/utils/auth/validateAccessToken'
 import styles from './Header.module.sass'
+
+const NoSSRShoppingCart = dynamic(() => import('../ShoppingCart'), { ssr: false })
 
 export const Header = async () => {
   const customer = await validateAccessToken()
@@ -23,7 +25,7 @@ export const Header = async () => {
       </nav>
       <div className={styles.Header__user}>
         {customer?.firstName ? (<p>Hola! {customer.firstName}</p>) : (<Link href="/login">Login</Link>)}
-        <ShoppingCart />
+        <NoSSRShoppingCart />
       </div>
     </header>)
 }
--- src/components/shared/ShoppingCart/ShoppingCart.tsx
+++ src/components/shared/ShoppingCart/ShoppingCart.tsx
@@ -2,12 +2,27 @@
 import { useState } from "react";
 import { useShoppingCart } from "app/hooks/useShoppingCart";
 import { FaShoppingCart } from "react-icons/fa";
+import { handleCreateCart } from "app/actions";
 import styles from './ShoppingCart.module.sass'
 
-export const ShoppingCart = () => {
+export default function ShoppingCart() {
   const { cart } = useShoppingCart();
   const [isOpen, setIsOpen] = useState(false);
   const handleOpen = () => setIsOpen(!isOpen);
+
+  const handleBuy = async () => {
+    try {
+      setIsBuying(true);
+      const checkoutUrl = await handleCreateCart(cart);
+      if(!checkoutUrl) throw new Error('Error creating checkout');
+      window.localStorage.removeItem('cart');
+      window.location.href = checkoutUrl;
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsBuying(false);
+    }
+  }
   return (
     <button className={styles.ShoppingCart} onClick={handleOpen}>
       <span className={styles.ShoppingCart__counter}>
@@ -24,7 +39,7 @@ export const ShoppingCart = () => {
               </>
             ))
           }
-          <button className={styles.ShoppingCart__buyButton}>
+          <button onClick={handleBuy}  className={styles.ShoppingCart__buyButton} disabled={isBuying}>
             Buy
           </button>
         </div>
